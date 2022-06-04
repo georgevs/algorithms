ee4dae46089ca8c726afadded773b7eb4cc29438
George <georgevs@gmail.com>
2022-06-04 18:24:35 +0300

    fcc algorithms search bfs test order

--- search/test.js
+++ search/test.js
@@ -15,19 +15,34 @@ seq 1 | xargs -L1 time node -e 'require("./test")(require(process.argv[1]),proce
 
 const { asserteq, UnorderedArray } = require('../asserteq');
 const { dagxs, vertices: [A, B, C, D, E, F, G, H] } = require('../sample-graph-data');
-const { dg } = require('../graph');
+const { g: graph, dg: directedGraph } = require('../graph');
 
-const dag = dg(dagxs);
+const dag = directedGraph(dagxs);
+
 const acc = (fn) => (...args) => { const rs = []; fn((v) => (rs.push(v), void 0), ...args); return rs };
 const loop = (n, fn) => { for (let i = 0; i < n; ++i) fn(i) };
 
-const test = ({ enumVertices, enumPaths }, n) => loop(Number.parseInt(n) || 1, () => {
+const test = ({ enumVertices, enumPaths, bfs }, n) => loop(Number.parseInt(n) || 1, () => {
   if (enumVertices) {
     const assertEnumVertices = (expected) => (g) => asserteq(expected, UnorderedArray.from(acc(enumVertices)(g)));
     [dag].forEach(assertEnumVertices([A, B, C, D, E, F, G, H]));
   }
 
   if (enumPaths) {
+  }
+
+  if (bfs?.enumVertices) {
+    const xs = [
+      [A, B], [A, D],
+      [B, A], [B, C], [B, E], [B, F],
+      [C, B], [C, D],
+      [D, A], [D, C], [D, E],
+      [E, B], [E, D], [E, F],
+      [F,B],[F,E]
+    ];
+
+    const assertEnumVertices = (enumVertices) => asserteq([A, B, D, C, E, F], acc(enumVertices)(graph(xs)));
+    bfs.enumVertices.forEach(assertEnumVertices);
   }
 });
 
