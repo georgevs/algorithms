2b4cb5ca6f4e1e2f46c3acc309bb1f89da564d48
George <georgevs@gmail.com>
2022-07-03 12:25:42 +0300

    fcc algorithms articulation points fcc

--- bridges-and-articulation-points/bridges-fcc.js
+++ bridges-and-articulation-points/bridges-fcc.js
@@ -7,23 +7,29 @@ const bridges = (g) => {
   const low = new Map;
   const bs = [];
   let id = 0;
-  const dfs = (v1, p) => {
+  
+  const dfs = (v1, vp) => {
     vs.add(v1);
     ids.set(v1, id);
     low.set(v1, id);
     ++id;
     for (const v2 of g.neighbours(v1)) {
-      if (v2 === p) { continue }
+      if (v2 === vp) { continue }
       if (!vs.has(v2)) {
         dfs(v2, v1);
         low.set(v1, Math.min(low.get(v1), low.get(v2)));
         if (ids.get(v1) < low.get(v2)) { bs.push([v1, v2]) }
       } else {
-        low.set(v1, ids.get(v2));
+        low.set(v1, Math.min(low.get(v1), ids.get(v2)));
       }
     }
   };
-  for (const v1 of g.vertices()) { if (!vs.has(v1)) { dfs(v1, null) } }
+
+  for (const v1 of g.vertices()) {
+    if (!vs.has(v1)) {
+      dfs(v1, null);
+    }
+  }
   return bs;
 };
 
