ac15e58b64ad866f585f6a690b28c9c751156f9f
George <georgevs@gmail.com>
2022-08-10 11:35:24 +0300

    fcc algorithms strongly connected components ...

--- traveling-salesman/test.js
+++ traveling-salesman/test.js
@@ -3,14 +3,35 @@ seq 1 | xargs -L1 time node -e 'require("./test")(require(process.argv[1]),proce
 */
 
 const { asserteq } = require('../asserteq');
-const { tspdgxs, vertices: [A, B, C, D, E, F, G, H] } = require('../sample-graph-data');
-const { directedGraph } = require('../graph');
 
-const tspdg = directedGraph(tspdgxs);
+const [A,B,C,D] = Array.from('ABCD').map(Symbol);
+const xs = [
+  [A,B,4],[A,C,1],[A,D,9],
+  [B,A,3],[B,C,6],[B,D,11],
+  [C,A,4],[C,B,1],[C,D,2],
+  [D,A,6],[D,B,5],[D,C,-4]
+];
+
 
 const loop = (n, fn) => { for (let i = 0; i < n; ++i) fn(i) };
-const test = ({ travelingSalesman }, n) => loop(Number.parseInt(n) || 1, () => {
-  asserteq([A,D,C,B], travelingSalesman(tspdg));
+const test = ({ adjacencyMatrix, directedWeightedGraph, travelingSalesman }, n) => loop(Number.parseInt(n) || 1, () => {
+  [directedWeightedGraph].filter(Boolean).forEach(testGraph);
+  [travelingSalesman].filter(Boolean).forEach(testTSS(require('./directed-weighted-graph')));
+  [adjacencyMatrix].filter(Boolean).forEach(testAM);
 });
+const testAM = (adjacencyMatrix) => {
+  console.log(adjacencyMatrix(xs));
+};
+const testTSS = (directedWeightedGraph) => (travelingSalesman) => {
+  asserteq([A,D,C,B], travelingSalesman(directedWeightedGraph(xs)));
+};
+const testGraph = (directedWeightedGraph) => {
+  asserteq('A/B 4,C 1,D 9;B/A 3,C 6,D 11;C/A 4,B 1,D 2;D/A 6,B 5,C -4', directedWeightedGraph(xs).toString());
+};
 
 module.exports = test;
+
+if (require.main === module) {
+  test({ directedWeightedGraph: require('./directed-weighted-graph') });
+  test({ adjacencyMatrix: require('./adjacency-matrix') });
+}
