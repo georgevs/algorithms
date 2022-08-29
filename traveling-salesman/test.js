f93b9b3064043b75e79ea1716c1a88a3e09ab65b
George <georgevs@gmail.com>
2022-08-29 15:46:08 +0300

    fcc algorithms traveling salesman min route path

--- traveling-salesman/test.js
+++ traveling-salesman/test.js
@@ -8,12 +8,13 @@ const m = [
 ];
 
 const loop = (n, fn) => { for (let i = 0; i < n; ++i) fn(i) };
-const test = ({ travelingSalesman }, n) => loop(Number.parseInt(n) || 1, () => {
-  asserteq(9, travelingSalesman(m, 0));
+const test = ({ minRouteCost, minRoutePath }, n) => loop(Number.parseInt(n) || 1, () => {
+  asserteq(9, minRouteCost(m, 0));
+  asserteq([0,3,2,1], minRoutePath(m, 0));
 });
 
 module.exports = test;
 
 if (require.main === module) {
-  test({ travelingSalesman: require('./travelingSalesman') });
+  test(require('./traveling-salesman'));
 }
